3739668a4da8fb3ac55fae401630a18d10ada0f8
Devarsh Patel <dev@Devarshs-MacBook-Pro.local>
2025-05-26 20:50:34 -0500

    Initial commit
    Add ThreeJS

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+// Styles
+import '@/app/App.css';
+
+// Components
+import ThreeMain from '@/app/components/3d';
+
+function App() {
+  return (
+    <>
+      <ThreeMain />
+      <div className="w-full h-screen overflow-scroll">
+        {/* <HTML Elements /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
